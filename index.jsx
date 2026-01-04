631b2009a8e8c7d7b6b6093e8195c437860fb923
Tejasri Teyam <tsree0421@gmail.com>
2026-01-04 19:37:38 +0530

    second commit

--- index.jsx
+++ index.jsx
@@ -1,26 +1,59 @@
 
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from "./components/Home.jsx"
-import About from './components/About.jsx'
 
 
+import Home from "./pages/Home.jsx"
+import About from './pages/About.jsx'
+import Vans from "./pages/Vans.jsx"
+import VanDetail from './pages/VanDetail.jsx';
+import Layout from './components/Layout.jsx'
+import Dashboard from './pages/Host/Dashboard.jsx';
+import Income from './pages/Host/Income.jsx';
+import Reviews from './pages/Host/Reviews.jsx'
+import HostLayout from './components/HostLayout.jsx';
+import HostVans from './pages/Host/HostVans.jsx';
+import HostVanDetail from './pages/Host/HostVanDetail.jsx';
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+
+
+import "./server"
 
 
 
 
 function App() {
+
+
+
+
+
+
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout/>} >
+            <Route index element={<Dashboard/>}/>
+            <Route path="income" element={<Income/>}/>
+            <Route path="reviews" element={<Reviews/>}/>
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} >
+              <Route index element={<HostVanInfo/>} />
+              <Route path="pricing" element={<HostVanPricing/>}/>
+              <Route path="photos" element={<HostVanPhotos/>}/>
+            </Route>
+          </Route>
+
+        </Route>
+
       </Routes>
     </BrowserRouter>
   )
---
+++ pages/Host/HostVanInfo.jsx
@@ -0,0 +1,16 @@
+import { useOutletContext } from "react-router"
+
+
+export default function HostVanInfo() {
+    // const [vanDesc,setVanDesc] = useOutletContext()
+    const {van} = useOutletContext()
+    console.log(van)
+    return (
+         <section className="host-van-detail-info">
+            <h4>Name: <span>{van.name}</span></h4>
+            <h4>Category: <span>{van.type}</span></h4>
+            <h4>Description: <span>{van.description}</span></h4>
+            <h4>Visibility: <span>Public</span></h4>
+        </section>
+    )
+}
