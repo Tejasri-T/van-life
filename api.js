ec3fb34b924b136503ff48bb339dd3c05acde589
Tejasri Teyam <tsree0421@gmail.com>
2026-01-16 20:57:55 +0530

    just small changes

--- api.js
+++ api.js
@@ -36,6 +36,22 @@ export async function getVan(id){
     }
 }
 
+
+// export async function getVans(id) {
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
+
+
 export async function getHostVans(){
     const q = query(vansCollectionRef,where("hostId","==","123"))
     const querySnapshot = await getDocs(q)
@@ -46,10 +62,6 @@ export async function getHostVans(){
    console.log(dataArr)
    return dataArr
 }
-
-
-
-
 
 
 
